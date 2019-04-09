4e774d8f9457309e122fd5f4dabc30b34559eee2
Blake <2979337714@qq.com>
2019-04-09 12:14:31 +0800

    添加上下一张及自动轮播

--- main.js
+++ main.js
@@ -6,9 +6,68 @@ let current = 0;
 makeFakeSlides();
 $images.css({ transform: "translateX(-168px)" });
 bindEvents();
+$(next).on('click',function (){
+  goToSlide(current+1)
+})
+$(previous).on('click',function (){
+  goToSlide(current-1)
+})
+
+let setTimer = setInterval(function (){
+  goToSlide(current+1)
+},2000)
+$('#window').on('mouseenter',function (){
+  window.clearInterval(setTimer)
+})
+$('#window').on('mouseleave',function (){
+  setTimer = setInterval(function (){
+    goToSlide(current+1)
+  },2000)
+})
+
+
+function goToSlide($index){
+  //此处有bug，以下代码并没有切换的问题
+  if($index>$buttons.length-1){
+    var $index = 0
+  }else if($index < 0){
+    var $index  = $buttons.length-1
+  }
+  console.log(current,$index)
+  if(current === $buttons.length-1 && $index === 0){
+    //从最后一张到第一张
+    console.log('从最后一张到第一张')
+    $images
+      .css({ transform: `translateX(${-168*($buttons.length+1)}px)` })
+      .one("transitionend", function() {
+        $images.hide().offset();
+        $images.css({ transform: "translateX(-168px)" }).show();
+      });
+  }else if(current === 0 && $index === $buttons.length-1){
+    //从第一张到最后一张
+    console.log('从第一张到最后一张')
+    $images
+    .css({ transform: 'translateX(-168px)' })
+    .one("transitionend", function() {
+      $images.hide().offset();
+      $images.css({ transform: `translateX(${-168*($index+1)}px)` }).show();
+    });
+  }else{
+    console.log('正常')
+    $images.css({ transform: `translateX(${-($index+1)*168}px)`});
+  }
+  current = $index
+}
 
 
 function bindEvents(){
+  $('#buttonWrapper').on('click','button',function (e){
+    let $button = $(e.currentTarget)
+    let $index = $button.index()
+    goToSlide($index)
+  })
+
+  /*
   $buttons.eq(0).on("click", function() {
     if (current == 3) {
       $images
@@ -43,7 +102,9 @@ function bindEvents(){
     }
     current = 3;
   });
+  */
 }
+
 
 function makeFakeSlides() {
     let $firstCopy = $img.eq(0).clone(true);
