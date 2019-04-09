421d76c7d9f027c0f59707a1692348dd8c8c6897
Blake <2979337714@qq.com>
2019-04-09 10:47:00 +0800

    format

--- main.js
+++ main.js
@@ -1,47 +1,53 @@
-let $buttons = $('#buttonWrapper>button')
-let $images = $('#images')
-let $img = $('#images').children('img')
-let $firstCopy = $img.eq(0).clone(true)
-let $lastCopy = $img.eq($img.length-1).clone(true)
-$images.append($firstCopy)
-$images.prepend($lastCopy)
+let $buttons = $("#buttonWrapper>button");
+let $images = $("#images");
+let $img = $("#images").children("img");
+let current = 0;
 
-let current = 0
-$images.css({transform:'translateX(-168px)'})
+makeFakeSlides();
+$images.css({ transform: "translateX(-168px)" });
+bindEvents();
 
-$buttons.eq(0).on('click', function (){
-    if(current == 3){
-        $images.css({transform:'translateX(-840px)'})
-            .one('transitionend', function (){
-                $images.hide()
-                    .offset()
-                $images.css({transform:'translateX(-168px)'})
-                    .show()
-            })
-    }else{
-        $images.css({transform:'translateX(-168px)'})
+
+function bindEvents(){
+  $buttons.eq(0).on("click", function() {
+    if (current == 3) {
+      $images
+        .css({ transform: "translateX(-840px)" })
+        .one("transitionend", function() {
+          $images.hide().offset();
+          $images.css({ transform: "translateX(-168px)" }).show();
+        });
+    } else {
+      $images.css({ transform: "translateX(-168px)" });
     }
-    current = 0
-})
-$buttons.eq(1).on('click', function (){
-    $images.css({transform:'translateX(-336px)'})
-    current = 1
-})
-$buttons.eq(2).on('click', function (){
-    $images.css({transform:'translateX(-504px)'})
-    current = 2
-})
-$buttons.eq(3).on('click', function (){
-    if(current == 0){
-        $images.css({transform:'translateX(0px)'})
-        .one('transitionend', function (){
-            $images.hide()
-                .offset()
-            $images.css({transform:'translateX(-672px)'})
-                .show()
-        }) 
-    }else{
-        $images.css({transform:'translateX(-672px)'})
+    current = 0;
+  });
+  $buttons.eq(1).on("click", function() {
+    $images.css({ transform: "translateX(-336px)" });
+    current = 1;
+  });
+  $buttons.eq(2).on("click", function() {
+    $images.css({ transform: "translateX(-504px)" });
+    current = 2;
+  });
+  $buttons.eq(3).on("click", function() {
+    if (current == 0) {
+      $images
+        .css({ transform: "translateX(0px)" })
+        .one("transitionend", function() {
+          $images.hide().offset();
+          $images.css({ transform: "translateX(-672px)" }).show();
+        });
+    } else {
+      $images.css({ transform: "translateX(-672px)" });
     }
-    current = 3
-})
+    current = 3;
+  });
+}
+
+function makeFakeSlides() {
+    let $firstCopy = $img.eq(0).clone(true);
+    let $lastCopy = $img.eq($img.length - 1).clone(true);
+    $images.append($firstCopy);
+    $images.prepend($lastCopy);
+  }
